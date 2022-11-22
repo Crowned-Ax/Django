151b914e88b451f33950eecf5820520345913e5c
Crowned-Ax <sebastr@yahoo.com>
2022-11-22 00:33:37 -0500

    intento 3.2

--- Tour/settings.py
+++ Tour/settings.py
@@ -15,12 +15,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import dj_database_url
 import os
-from django.test.utils import ignore_warnings
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ignore_warnings(message="No directory at", module="whitenoise.base").enable()
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
